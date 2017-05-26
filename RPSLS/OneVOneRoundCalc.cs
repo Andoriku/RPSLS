0e99cb57a73a2497e42a3d9711fe49a04173e666
Andrew Jordan <andoriku@gmail.com>
2017-05-26 09:23:34 -0500

    added player2, computer, and the 1v1 and 1vC calculation

---
+++ RPSLS/OneVOneRoundCalc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class OneVOneRoundCalc
+    {
+        public string outcome;
+        public string whoWon(int p1MoveChoice, int compChoice)
+        {
+            int result;
+
+            result = (5 + p1MoveChoice - compChoice) % 5;
+            switch (result)
+            {
+                case 1:
+                    outcome = "win";
+                    return outcome;
+                case 2:
+                    outcome = "lose";
+                    return outcome;
+                case 3:
+                    outcome = "win";
+                    return outcome;
+                case 4:
+                    outcome = "lose";
+                    return outcome;
+                case 0:
+                    outcome = "tie";
+                    return outcome;
+                default:
+                    outcome = "problem";
+                    return outcome;
+            }
+        }
+}
---
+++ RPSLS/Users.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLS
+{
+    public class Users
+    {
+        public int p1MoveChoice;
+        public int p2MoveChoice;
+        public string playerName;
+        public int compChoice;
+
+        public string MakePlayerName()
+        {
+            Console.WriteLine("What is your name? Enter name below");
+            playerName = Console.ReadLine();
+            return playerName;
+        }
+        public int p1MakeChoice()
+        {
+            Console.WriteLine("what do you want to throw? 'rock','paper','scissor','lizard', or 'spock'");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "rock":
+                    p1MoveChoice = 1;
+                    return p1MoveChoice;
+                case "paper":
+                    p1MoveChoice = 2;
+                    return p1MoveChoice;
+                case "scissor":
+                    p1MoveChoice = 3;
+                    return p1MoveChoice;
+                case "lizard":
+                    p1MoveChoice = 4;
+                    return p1MoveChoice;
+                case "spock":
+                    p1MoveChoice = 5;
+                    return p1MoveChoice;
+                default:
+                    Console.WriteLine("You have to pick one of these: 'rock','paper','scissor','lizard', or 'spock'");
+                    Console.Read();
+                    return p1MakeChoice();
+
+            }
+        }
+        public int p2MakeChoice()
+        {
+            Console.WriteLine("what do you want to throw? 'rock','paper','scissor','lizard', or 'spock'");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "rock":
+                    p1MoveChoice = 1;
+                    return p2MoveChoice;
+                case "paper":
+                    p1MoveChoice = 2;
+                    return p2MoveChoice;
+                case "scissor":
+                    p1MoveChoice = 3;
+                    return p2MoveChoice;
+                case "lizard":
+                    p1MoveChoice = 4;
+                    return p2MoveChoice;
+                case "spock":
+                    p1MoveChoice = 5;
+                    return p2MoveChoice;
+                default:
+                    Console.WriteLine("You have to pick one of these: 'rock','paper','scissor','lizard', or 'spock'");
+                    Console.Read();
+                    return p2MakeChoice();
+
+            }
+        }
+
+        Random random = new Random();
+        public int GetCompChoice()
+        {
+            compChoice = random.Next(0, 5);
+            return compChoice;
+        }
+    }
+}
